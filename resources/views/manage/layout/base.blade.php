72eaed66e46feed5250f27535f9eadd984137b36
lilichun <1077341744@qq.com>
2016-02-02 13:33:26 +0800

    update layout view

--- resources/views/manage/layout/base.blade.php
+++ resources/views/manage/layout/base.blade.php
@@ -9,7 +9,7 @@
     <meta http-equiv="cache-control" content="no-siteapp"/>
     <meta name="keywords" content="微信管理系统">
     <meta name="description" content="微信管理系统，是一款由国人开发的系统，完全免费开源。">
-    <title>wx-admin</title>
+    <title>wx-admin - @section('title') @stop</title>
     <link rel="Bookmark" href="/favicon.ico">
     <LINK rel="Shortcut Icon" href="/favicon.ico"/>
     <!--[if lt IE 9]>
@@ -18,20 +18,18 @@
     ]) !!}
     <![endif]-->
     {!! link_css('static/package/H-ui', [
-        'css/H-ui.min.css', 'css/H-ui.admin.css', 'lib/Hui-iconfont/1.0.6/iconfont.css', 'skin/default/skin.css',
-        'css/style.css', 'css/H-ui.min.css', 'css/H-ui.min.css', 'lib/Hui-iconfont/1.0.6/iconfont.css', 'skin/default/skin.css', 'css/style.css'
+        'css/H-ui.min.css', 'css/H-ui.admin.css', 'lib/Hui-iconfont/1.0.6/iconfont.css', 'css/style.css',
     ]) !!}
+    {!! link_css('static/css', 'admin-app.css') !!}
     @section('css')
     @show
 </head>
 <body>
-@include('manage.widget.header')
-@include('manage.widget.menu')
 @yield('content')
 {!! link_script('static/package/H-ui', [
     'lib/jquery/1.9.1/jquery.min.js', 'lib/layer/2.1/layer.js', 'js/H-ui.js', 'js/H-ui.admin.js'
 ]) !!}
-{!! link_script('static/js', 'app.js') !!}
+{!! link_script('static/js', 'admin-app.js') !!}
 @section('js')
 @show
 </body>
