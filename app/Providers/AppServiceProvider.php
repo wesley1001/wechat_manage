3359d0602cf7590cc1a97f4341bf2e6c142295b2
lilichun <1077341744@qq.com>
2016-02-02 13:35:16 +0800

    interface binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $bindingList = [
+        \App\Contracts\ICustomCache::class => \App\Realizes\CustomCache::class
+    ];
+
     /**
      * Bootstrap any application services.
      *
@@ -23,6 +27,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->bindingInterface();
+    }
+
+    /**
+     * 绑定接口
+     */
+    public function bindingInterface()
+    {
+        foreach ($this->bindingList as $interface => $realize) {
+            $this->app->bind($interface, $realize);
+        }
     }
 }
